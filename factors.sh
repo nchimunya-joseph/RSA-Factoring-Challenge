fe48baa281b92334ebebf9add59938acfc0fbb93
nhcimunyascripts <nchimunya.backlogcode@gmail.com>
2024-01-27 13:30:51 +0200

    hard fix

---
+++ factors.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+if [ "$#" -ne 1 ]; then
+	echo "Usage: $0 <file>"
+	exit 1
+fi
+
+file="$1"
+
+while read -r number || [ -n "$number" ]; do
+	divisor=2
+	while [ $divisor -le $(( number / 2)) ]; do
+		if [ $((number % divisor)) -eq 0 ]; then
+			factor1=$divisor
+			factor2=$((number / divisor))
+			echo "$number=$factor1*$factor2"
+			break
+		fi
+		divisor=$((divisor + 1))
+	done
+done < "$file"
